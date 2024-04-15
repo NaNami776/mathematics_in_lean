1cc0d00444e42f7dd33dfa5b4bad87d93c599702
NaNami776 <129564082+NaNami776@users.noreply.github.com>
2024-04-14 21:49:22 -0700

    Update Solutions_S01_Implication_and_the_Universal_Quantifier.lean

--- MIL/C03_Logic/solutions/Solutions_S01_Implication_and_the_Universal_Quantifier.lean
+++ MIL/C03_Logic/solutions/Solutions_S01_Implication_and_the_Universal_Quantifier.lean
@@ -3,14 +3,49 @@ import Mathlib.Data.Real.Basic
 
 namespace C03S01
 
+#check ∀ x : ℝ, 0 ≤ x → |x| = x
+
+#check ∀ x y ε : ℝ, 0 < ε → ε ≤ 1 → |x| < ε → |y| < ε → |x * y| < ε
+
+theorem my_lemma : ∀ x y ε : ℝ, 0 < ε → ε ≤ 1 → |x| < ε → |y| < ε → |x * y| < ε :=
+  sorry
+
+section
+variable (a b δ : ℝ)
+variable (h₀ : 0 < δ) (h₁ : δ ≤ 1)
+variable (ha : |a| < δ) (hb : |b| < δ)
+
+#check my_lemma a b δ
+#check my_lemma a b δ h₀ h₁
+#check my_lemma a b δ h₀ h₁ ha hb
+
+end
+
+theorem my_lemma2 : ∀ {x y ε : ℝ}, 0 < ε → ε ≤ 1 → |x| < ε → |y| < ε → |x * y| < ε :=
+  sorry
+
+section
+variable (a b δ : ℝ)
+variable (h₀ : 0 < δ) (h₁ : δ ≤ 1)
+variable (ha : |a| < δ) (hb : |b| < δ)
+
+#check my_lemma2 h₀ h₁ ha hb
+
+end
+
+theorem my_lemma3 :
+    ∀ {x y ε : ℝ}, 0 < ε → ε ≤ 1 → |x| < ε → |y| < ε → |x * y| < ε := by
+  intro x y ε epos ele1 xlt ylt
+  sorry
+
 theorem my_lemma4 :
     ∀ {x y ε : ℝ}, 0 < ε → ε ≤ 1 → |x| < ε → |y| < ε → |x * y| < ε := by
   intro x y ε epos ele1 xlt ylt
   calc
-    |x * y| = |x| * |y| := by apply abs_mul
-    _ ≤ |x| * ε := by apply mul_le_mul; linarith; linarith; apply abs_nonneg; apply abs_nonneg;
-    _ < 1 * ε := by rw [mul_lt_mul_right epos]; linarith
-    _ = ε := by apply one_mul
+    |x * y| = |x| * |y| := sorry
+    _ ≤ |x| * ε := sorry
+    _ < 1 * ε := sorry
+    _ = ε := sorry
 
 def FnUb (f : ℝ → ℝ) (a : ℝ) : Prop :=
   ∀ x, f x ≤ a
@@ -21,48 +56,58 @@ def FnLb (f : ℝ → ℝ) (a : ℝ) : Prop :=
 section
 variable (f g : ℝ → ℝ) (a b : ℝ)
 
-example (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) := by
+example (hfa : FnUb f a) (hgb : FnUb g b) : FnUb (fun x ↦ f x + g x) (a + b) := by
   intro x
+  dsimp
   apply add_le_add
   apply hfa
   apply hgb
 
-example (nnf : FnLb f 0) (nng : FnLb g 0) : FnLb (fun x ↦ f x * g x) 0 := by
-  intro x
-  apply mul_nonneg
-  apply nnf
-  apply nng
+example (hfa : FnLb f a) (hgb : FnLb g b) : FnLb (fun x ↦ f x + g x) (a + b) :=
+  sorry
+
+example (nnf : FnLb f 0) (nng : FnLb g 0) : FnLb (fun x ↦ f x * g x) 0 :=
+  sorry
 
 example (hfa : FnUb f a) (hgb : FnUb g b) (nng : FnLb g 0) (nna : 0 ≤ a) :
-    FnUb (fun x ↦ f x * g x) (a * b) := by
-  intro x
-  apply mul_le_mul
-  apply hfa
-  apply hgb
-  apply nng
-  apply nna
+    FnUb (fun x ↦ f x * g x) (a * b) :=
+  sorry
 
 end
 
 section
+variable {α : Type*} {R : Type*} [OrderedCancelAddCommMonoid R]
+
+#check add_le_add
+
+def FnUb' (f : α → R) (a : R) : Prop :=
+  ∀ x, f x ≤ a
+
+theorem fnUb_add {f g : α → R} {a b : R} (hfa : FnUb' f a) (hgb : FnUb' g b) :
+    FnUb' (fun x ↦ f x + g x) (a + b) := fun x ↦ add_le_add (hfa x) (hgb x)
+
+end
+
+example (f : ℝ → ℝ) (h : Monotone f) : ∀ {a b}, a ≤ b → f a ≤ f b :=
+  @h
+
+section
 variable (f g : ℝ → ℝ)
 
-example {c : ℝ} (mf : Monotone f) (nnc : 0 ≤ c) : Monotone fun x ↦ c * f x := by
+example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f x + g x := by
   intro a b aleb
-  apply mul_le_mul_of_nonneg_left _ nnc
+  apply add_le_add
   apply mf aleb
+  apply mg aleb
+
+example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f x + g x :=
+  fun a b aleb ↦ add_le_add (mf aleb) (mg aleb)
 
 example {c : ℝ} (mf : Monotone f) (nnc : 0 ≤ c) : Monotone fun x ↦ c * f x :=
-  fun a b aleb ↦ mul_le_mul_of_nonneg_left (mf aleb) nnc
-
-example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f (g x) := by
-  intro a b aleb
-  apply mf
-  apply mg
-  apply aleb
+  sorry
 
 example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f (g x) :=
-  fun a b aleb ↦ mf (mg aleb)
+  sorry
 
 def FnEven (f : ℝ → ℝ) : Prop :=
   ∀ x, f x = f (-x)
@@ -70,22 +115,21 @@ def FnEven (f : ℝ → ℝ) : Prop :=
 def FnOdd (f : ℝ → ℝ) : Prop :=
   ∀ x, f x = -f (-x)
 
-example (of : FnOdd f) (og : FnOdd g) : FnEven fun x ↦ f x * g x := by
+example (ef : FnEven f) (eg : FnEven g) : FnEven fun x ↦ f x + g x := by
   intro x
   calc
-    (fun x ↦ f x * g x) x = f x * g x := rfl
-    _ = f (-x) * g (-x) := by rw [of, og, neg_mul_neg]
+    (fun x ↦ f x + g x) x = f x + g x := rfl
+    _ = f (-x) + g (-x) := by rw [ef, eg]
 
+
+example (of : FnOdd f) (og : FnOdd g) : FnEven fun x ↦ f x * g x := by
+  sorry
 
 example (ef : FnEven f) (og : FnOdd g) : FnOdd fun x ↦ f x * g x := by
-  intro x
-  dsimp
-  rw [ef, og, neg_mul_eq_mul_neg]
+  sorry
 
 example (ef : FnEven f) (og : FnOdd g) : FnEven fun x ↦ f (g x) := by
-  intro x
-  dsimp
-  rw [og, ← ef]
+  sorry
 
 end
 
@@ -93,14 +137,14 @@ section
 
 variable {α : Type*} (r s t : Set α)
 
-example : r ⊆ s → s ⊆ t → r ⊆ t := by
-  intro rsubs ssubt x xr
-  apply ssubt
-  apply rsubs
-  apply xr
+example : s ⊆ s := by
+  intro x xs
+  exact xs
 
-theorem Subset.trans : r ⊆ s → s ⊆ t → r ⊆ t :=
-  fun rsubs ssubt x xr ↦ ssubt (rsubs xr)
+theorem Subset.refl : s ⊆ s := fun x xs ↦ xs
+
+theorem Subset.trans : r ⊆ s → s ⊆ t → r ⊆ t := by
+  sorry
 
 end
 
@@ -111,12 +155,8 @@ variable (s : Set α) (a b : α)
 def SetUb (s : Set α) (a : α) :=
   ∀ x, x ∈ s → x ≤ a
 
-example (h : SetUb s a) (h' : a ≤ b) : SetUb s b := by
-  intro x xs
-  apply le_trans (h x xs) h'
-
 example (h : SetUb s a) (h' : a ≤ b) : SetUb s b :=
-  fun x xs ↦ le_trans (h x xs) h'
+  sorry
 
 end
 
@@ -124,17 +164,17 @@ section
 
 open Function
 
-example {c : ℝ} (h : c ≠ 0) : Injective fun x ↦ c * x := by
+example (c : ℝ) : Injective fun x ↦ x + c := by
   intro x₁ x₂ h'
-  apply (mul_right_inj' h).mp h'
+  exact (add_left_inj c).mp h'
+
+example {c : ℝ} (h : c ≠ 0) : Injective fun x ↦ c * x := by
+  sorry
 
 variable {α : Type*} {β : Type*} {γ : Type*}
 variable {g : β → γ} {f : α → β}
 
 example (injg : Injective g) (injf : Injective f) : Injective fun x ↦ g (f x) := by
-  intro x₁ x₂ h
-  apply injf
-  apply injg
-  apply h
+  sorry
 
 end
